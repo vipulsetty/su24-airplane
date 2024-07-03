88a31ccff5909ff4d5ff32cd5e2d2bc58b6d3dd8
Chris Murphy <cdmurphy@seas.upenn.edu>
2024-07-03 10:08:30 -0400

    updated directory

--- src/airplane/sim/GameConfig.java
+++ src/airplane/sim/GameConfig.java
@@ -161,7 +161,7 @@ public class GameConfig implements Cloneable{
 			}
 		}
 		
-		File sourceFolder = new File("out/production/airplane-test/airplane/");
+		File sourceFolder = new File("out/production/su24-airplane/airplane/");
 		for(File f : sourceFolder.listFiles())
 		{
 			if(f.getName().length() == 2 && f.getName().substring(0,1).equals("g"))
