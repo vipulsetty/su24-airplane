c5d1c8dbde401b0b5050b4e909ba4599fd5d68fc
vipulsetty <saivipul03@gmail.com>
2024-07-09 22:00:56 -0400

    created second player

--- src/airplane/g0/group2Player.java
+++ src/airplane/g0/group2Player.java
@@ -2,16 +2,18 @@ package airplane.g0;
 
 import java.util.ArrayList;
 import java.lang.Math;
+
+import airplane.sim.SimulationResult;
 import org.apache.log4j.Logger;
 import airplane.sim.Plane;
 import airplane.sim.Player;
 
-public class newPlayer extends Player {
+public class group2Player extends Player {
     private Logger logger = Logger.getLogger(this.getClass()); // for logging
 
     @Override
     public String getName() {
-        return "new player";
+        return "group2 player";
     }
 
     @Override
@@ -28,46 +30,59 @@ public class newPlayer extends Player {
                 Plane curPlane=planes.get(i);
                 if(curPlane.getBearing()==-1){
                     bearings[i] = calculateBearing(curPlane.getLocation(), curPlane.getDestination());
+                    planes.get(i).setBearing(bearings[i]);
                 }
 
             }
         }
 
-        if(distance(planes.get(0),planes.get(1))<15 ){
+        SimulationResult simulation = this.startSimulation(planes,round);
+        logger.info(simulation.getReason());
+
+
+        if(simulation.getReason()==4 && simulation.getRound()-round<=7){
             bearings[0]+=10;
             bearings[1]+=10;
         }
 
-        if(distance(planes.get(0),planes.get(1))>=15 ){
+        if(simulation.getReason()!=4){
             double newBearing=calculateBearing(planes.get(0).getLocation(), planes.get(0).getDestination());
             if (Math.abs(newBearing-planes.get(0).getBearing())>10 && round>10){
                 bearings[0]=planes.get(0).getBearing()-10;
+                planes.get(0).setBearing(bearings[0]);
             }
             else{
                 bearings[0]=newBearing;
+                planes.get(0).setBearing(bearings[0]);
             }
 
             newBearing=calculateBearing(planes.get(1).getLocation(), planes.get(1).getDestination());
             if (Math.abs(newBearing-planes.get(1).getBearing())>10 && round>10){
                 bearings[1]=planes.get(1).getBearing()-10;
+                planes.get(1).setBearing(bearings[1]);
             }
             else{
                 bearings[1]=newBearing;
+                planes.get(1).setBearing(bearings[1]);
             }
         }
 
         if(bearings[0]>360){
             bearings[0]-=360;
+            planes.get(0).setBearing(bearings[0]);
         }
         if(bearings[1]>360){
             bearings[1]-=360;
+            planes.get(1).setBearing(bearings[1]);
         }
 
         if(bearings[0]<0 && round>10){
             bearings[0]+=360;
+            planes.get(0).setBearing(bearings[0]);
         }
         if(bearings[1]<0 && round>10){
             bearings[1]+=360;
+            planes.get(1).setBearing(bearings[1]);
         }
 
         return bearings;
@@ -77,4 +92,11 @@ public class newPlayer extends Player {
         double out = plane1.getLocation().distance(plane2.getLocation());
         return out;
     }
+
+    @Override
+    protected double[] simulateUpdate(ArrayList<Plane> planes, int round, double[] bearings) {
+        // not implemented
+        return bearings;
+    }
+
 }
