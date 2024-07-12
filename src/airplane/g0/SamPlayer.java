0d4bc39fd4279c414be6329b8edd2c310c076d63
spolloc <spolloc@seas.upenn.edu>
2024-07-12 09:49:26 -0400

    SamAirplane

---
+++ src/airplane/g0/SamPlayer.java
@@ -0,0 +1,104 @@
+package airplane.g0;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.awt.geom.Point2D;
+import java.lang.Math;
+
+import airplane.sim.SimulationResult;
+import org.apache.log4j.Logger;
+import airplane.sim.Plane;
+import airplane.sim.Player;
+
+public class SamPlayer extends Player {
+    private Logger logger = Logger.getLogger(this.getClass());
+    private Map<Point2D.Double, Integer> lastTakeoffTime = new HashMap<>();
+
+    @Override
+    public String getName() {
+        return "Sam player";
+    }
+
+    @Override
+    public void startNewGame(ArrayList<Plane> planes) {
+        logger.info("Starting new game!");
+        lastTakeoffTime.clear();
+    }
+
+
+    @Override
+    public double[] updatePlanes(ArrayList<Plane> planes, int round, double[] bearings) {
+        // Handle takeoffs with staggering
+        for (int i = 0; i < planes.size(); i++) {
+            Plane plane = planes.get(i);
+            if (round >= plane.getDepartureTime() && plane.getBearing() == -1) {
+                Point2D.Double destination = plane.getDestination();
+                if (!lastTakeoffTime.containsKey(destination) || round >= lastTakeoffTime.get(destination) + 4) {
+                    // Allow takeoff
+                    bearings[i] = calculateBearing(plane.getLocation(), destination);
+                    plane.setBearing(bearings[i]);
+                    lastTakeoffTime.put(destination, round);
+                } else {
+                    // Delay takeoff
+                    bearings[i] = -1;
+                }
+            }
+        }
+
+        // Collision avoidance for planes in the air
+        ArrayList<Plane> planesInAir = new ArrayList<>();
+        ArrayList<Integer> planesInAirIndices = new ArrayList<>();
+        for (int i = 0; i < planes.size(); i++) {
+            if (planes.get(i).getBearing() >= 0) {
+                planesInAir.add(planes.get(i));
+                planesInAirIndices.add(i);
+            }
+        }
+
+        if (planesInAir.size() > 1) {
+            SimulationResult simulation = this.startSimulation(planesInAir, round);
+            logger.info(simulation.getReason());
+
+            if (simulation.getReason() == 4 && simulation.getRound() - round <= 15) {
+                for (int i = 0; i < planesInAir.size(); i++) {
+                    int index = planesInAirIndices.get(i);
+                    bearings[index] += 10;
+                }
+            } else {
+                for (int i = 0; i < planesInAir.size(); i++) {
+                    int index = planesInAirIndices.get(i);
+                    Plane plane = planesInAir.get(i);
+                    double newBearing = calculateBearing(plane.getLocation(), plane.getDestination());
+                    if (Math.abs(newBearing - plane.getBearing()) > 10 && round > 10) {
+                        bearings[index] = plane.getBearing() - 10;
+                    } else {
+                        bearings[index] = newBearing;
+                    }
+                }
+            }
+        }
+
+        // Ensure bearings are within 0-360 range
+        for (int i = 0; i < bearings.length; i++) {
+            if (bearings[i] >= 0 && bearings[i] != -1 && bearings[i] != -2) {
+                bearings[i] = bearings[i] % 360;
+            } else if (bearings[i] < 0 && bearings[i] != -1 && bearings[i] != -2) {
+                bearings[i] = ((bearings[i] % 360) + 360) % 360;
+            }
+            planes.get(i).setBearing(bearings[i]);
+        }
+
+        return bearings;
+    }
+
+    public double distance(Plane plane1, Plane plane2) {
+        return plane1.getLocation().distance(plane2.getLocation());
+    }
+
+    @Override
+    protected double[] simulateUpdate(ArrayList<Plane> planes, int round, double[] bearings) {
+        // not implemented
+        return bearings;
+    }
+}
